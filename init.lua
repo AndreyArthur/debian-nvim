e4e6478f70f6e9d0ab54b783b1badd1487227f16
Andrey Arthur <andrey_arthur1@outlook.com>
2025-08-01 19:31:00 -0300

    bootstrap plugin manager

--- init.lua
+++ init.lua
@@ -53,3 +53,24 @@ vim.api.nvim_create_autocmd('VimEnter', {
         vim.api.nvim_set_current_dir(vim.fn.expand(CWD)) 
     end,
 })
+
+-- auto install lazy.nvim package manager
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=11.17.1', lazyrepo, lazypath }) -- this version is locked use 'stable' if you want updates
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+            { out, 'WarningMsg' },
+            { '\nPress any key to exit...' },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+local lazy = require('lazy')
+
+lazy.setup({ })
