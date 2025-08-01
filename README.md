8f8b663bd00360cab0f5ddce01a6a4420b29b9aa
Andrey Arthur <andrey_arthur1@outlook.com>
2025-08-01 19:28:35 -0300

    add native neovim configuration

--- README.md
+++ README.md
@@ -63,3 +63,73 @@ rm -rf neovim
 ```
 
 Cool, now we have installed all the software we need, let's start configuring neovim.
+
+## Neovim configuration.
+
+Let's configure neovim step by step, remember that neovim configurations are not settings, but scripts, so, you need to reopen neovim for it to load the new config.
+
+So open neovim in your init.lua:
+
+```sh
+nvim ~/.config/nvim/init.lua
+```
+
+Starting with basic native neovim configs:
+
+```lua
+vim.opt.number = true -- allow line numbers
+vim.opt.relativenumber = true -- allow relative line numbers
+
+vim.opt.swapfile = false -- don't save swap files
+vim.opt.backup = false -- don't backup any written files
+vim.opt.writebackup = false -- don't backup current file
+vim.opt.hidden = false -- don't allow to exit buffer without saving
+vim.opt.undofile = true -- persist undo history
+
+vim.opt.splitbelow = true -- always create new window below when horizontal splitting
+vim.opt.splitright = true -- always create new window right when vertical splitting
+
+vim.opt.expandtab = true -- use tabs as spaces
+vim.opt.tabstop = 4 -- show each tab as 4 spaces
+vim.opt.softtabstop = 4 -- treat every 4 spaces as a tab
+
+vim.opt.shiftwidth = 0 -- set characters for each indent level as equal to the tabstop value
+vim.opt.autoindent = true -- start new lines with same indentation as the above line
+vim.opt.smartindent = true -- automatically detect new indentation levels inside delimiters
+vim.opt.wrap = false -- disable line wrapping (breaking)
+
+vim.opt.signcolumn = 'yes' -- always show the signcolumn to avoid flickering
+vim.opt.cursorline = true -- highlight current cursor line
+vim.opt.colorcolumn = { 81, 121 } -- columns to indicate line width
+vim.opt.background = 'dark' -- color option
+
+vim.opt.fileencoding = 'utf-8' -- set default encoding
+vim.opt.mouse = '' -- disable mouse
+vim.opt.showtabline = 2 -- always show tabline
+
+vim.opt.winborder = 'rounded' -- set all borders to be rounded
+
+vim.g.mapleader = ' ' -- set space as leader key
+
+local opts = { noremap = true } -- set the default keymap options to no remap
+
+vim.keymap.set('n', '<leader>tt', '<cmd>terminal<cr>', opts) -- enter in terminal mode
+vim.keymap.set('t', '<esc>', '<c-\\><c-n>', opts) -- esc in terminal mode
+
+-- highlight when yanking text
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function ()
+        vim.highlight.on_yank({ higroup = 'Visual' })
+    end,
+})
+
+local CWD = '%:p:h' -- set neovim cwd string as a constant
+
+-- always change neovim dir do the current working directory when entering
+vim.api.nvim_create_autocmd('VimEnter', {
+    pattern = '*',
+    callback = function ()
+        vim.api.nvim_set_current_dir(vim.fn.expand(CWD)) 
+    end,
+})
+```
--- init.lua
+++ init.lua
@@ -1,45 +1,55 @@
-vim.opt.number = true -- line numbers: allowed
-vim.opt.relativenumber = true -- relative line numbers: allowed
+vim.opt.number = true -- allow line numbers
+vim.opt.relativenumber = true -- allow relative line numbers
 
-vim.opt.swapfile = false -- save swap files: not allowed
-vim.opt.backup = false -- backup all written files: not allowed
-vim.opt.writebackup = false -- backup current file: not allowed
-vim.opt.hidden = false -- exit buffer without saving: not allowed
-vim.opt.undofile = true -- persist undo history in file: allowed
+vim.opt.swapfile = false -- don't save swap files
+vim.opt.backup = false -- don't backup any written files
+vim.opt.writebackup = false -- don't backup current file
+vim.opt.hidden = false -- don't allow to exit buffer without saving
+vim.opt.undofile = true -- persist undo history
 
-vim.opt.splitbelow = true -- create a window below when splitting: always
-vim.opt.splitright = true -- create a window right when splitting: always
+vim.opt.splitbelow = true -- always create new window below when horizontal splitting
+vim.opt.splitright = true -- always create new window right when vertical splitting
 
-vim.opt.expandtab = true -- tabs as spaces: allowed
-vim.opt.tabstop = 4 -- spaces for each tab when showing: 4
-vim.opt.softtabstop = 4 -- spaces for each tab when editing: 4
+vim.opt.expandtab = true -- use tabs as spaces
+vim.opt.tabstop = 4 -- show each tab as 4 spaces
+vim.opt.softtabstop = 4 -- treat every 4 spaces as a tab
 
-vim.opt.shiftwidth = 0 -- characters for each indent level: use tabstop value
-vim.opt.autoindent = true -- start newlines with same indentation as above: allowed
-vim.opt.smartindent = true -- automatically detect new indentation levels: allowed
-vim.opt.wrap = false -- line wrapping: nowrap
+vim.opt.shiftwidth = 0 -- set characters for each indent level as equal to the tabstop value
+vim.opt.autoindent = true -- start new lines with same indentation as the above line
+vim.opt.smartindent = true -- automatically detect new indentation levels inside delimiters
+vim.opt.wrap = false -- disable line wrapping (breaking)
 
-vim.opt.signcolumn = 'yes' -- show signcolumn: always
-vim.opt.cursorline = true -- highlight current cursor line: allowed 
-vim.opt.colorcolumn = { 81, 121 } -- columns to indicate line width: 80, 120
-vim.opt.background = 'dark' -- background theme: dark
+vim.opt.signcolumn = 'yes' -- always show the signcolumn to avoid flickering
+vim.opt.cursorline = true -- highlight current cursor line
+vim.opt.colorcolumn = { 81, 121 } -- columns to indicate line width
+vim.opt.background = 'dark' -- color option
 
-vim.opt.fileencoding = 'utf-8' -- default encoding: utf-8
-vim.opt.mouse = '' -- mouse option: disabled
-vim.opt.showtabline = 2 -- tabline option: always
+vim.opt.fileencoding = 'utf-8' -- set default encoding
+vim.opt.mouse = '' -- disable mouse
+vim.opt.showtabline = 2 -- always show tabline
+
+vim.opt.winborder = 'rounded' -- set all borders to be rounded
 
 vim.g.mapleader = ' ' -- set space as leader key
 
-local opts = { noremap = true }
+local opts = { noremap = true } -- set the default keymap options to no remap
 
-vim.keymap.set('n', '<leader>r', '<cmd>source<cr>', opts) -- reload config
+vim.keymap.set('n', '<leader>tt', '<cmd>terminal<cr>', opts) -- enter in terminal mode
 vim.keymap.set('t', '<esc>', '<c-\\><c-n>', opts) -- esc in terminal mode
 
 -- highlight when yanking text
 vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function ()
         vim.highlight.on_yank({ higroup = 'Visual' })
-    end
+    end,
 })
 
-vim.cmd.colorscheme('unokai') -- set colorscheme
+local CWD = '%:p:h' -- set neovim cwd string as a constant
+
+-- always change neovim dir do the current working directory when entering
+vim.api.nvim_create_autocmd('VimEnter', {
+    pattern = '*',
+    callback = function ()
+        vim.api.nvim_set_current_dir(vim.fn.expand(CWD)) 
+    end,
+})
